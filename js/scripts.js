14d9620003fd9467760105bbd45f950510affe78
Lia Carpeneti and Louis Rios <louis@>
2017-05-24 19:56:05 -0700

    add images

--- js/scripts.js
+++ js/scripts.js
@@ -2,20 +2,33 @@ $(document).ready(function() {
   $("button#Meow").click(function() {
     $("ul#user").prepend("<li>Cat</li>");
     $("ul#webpage").prepend("<li>Meow</li>");
-    $("ul#user").children("li").first().click(function() {
-  alert('hi');
-});
+    $("#cat-showing").show()
+    $("#bone-showing").hide()
+    $("#dog-showing").hide()
+    $("#nip-showing").hide()
   });
   $("button#Woof").click(function() {
     $("ul#user").prepend("<li>Dog</li>");
     $("ul#webpage").prepend("<li>Woof</li>");
+    $("#dog-showing").show()
+    $("#cat-showing").hide()
+    $("#bone-showing").hide()
+    $("#nip-showing").hide()
   });
   $("button#Yum").click(function() {
     $("ul#user").prepend("<li>Bone</li>");
     $("ul#webpage").prepend("<li>Yum</li>");
+    $("#bone-showing").show()
+    $("#cat-showing").hide()
+    $("#dog-showing").hide()
+    $("#nip-showing").hide()
   });
   $("button#Purr").click(function() {
     $("ul#user").prepend("<li>Catnip</li>");
     $("ul#webpage").prepend("<li>Purr</li>");
+    $("#nip-showing").show()
+    $("#cat-showing").hide()
+    $("#dog-showing").hide()
+    $("#bone-showing").hide()
   });
 });
